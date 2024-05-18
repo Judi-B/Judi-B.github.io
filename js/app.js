2f6451467b4e664099c535138a21c3c49bba649f
Judi | Programmer <75368942+Electronic-mind@users.noreply.github.com>
2024-05-18 17:10:41 +0300

    .

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
-
+/*
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
 });
@@ -16,7 +16,7 @@ footer_input.addEventListener("blur", () => {
   if (footer_input.value != "") return;
   footer_input.classList.remove("focus");
 });
-
+*/
 function closeMenu() {
   navbar.classList.remove("open");
   document.body.classList.remove("stop-scrolling");
